961732f02893af60353197d390409a0b4ae3321d
502 <tencchi@hotmail.com>
2024-12-22 21:17:45 -0800

    ad

---
+++ src/main/java/com/example/filemanagement/fmbackend/FileManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.filemanagement.fmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/filemanagement/fmbackend/FileManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filemanagement.fmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
